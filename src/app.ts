6c0ba4876d70ad2a4ab0dd8c5a663190be16e1af
chun chen lai <pcwinalan@outlook.com>
2024-11-09 13:51:55 +0800

    fix. 暫時停用https（待修正）

--- src/app.ts
+++ src/app.ts
@@ -27,23 +27,28 @@ app.use('/users', UserRouter);
 app.use('/tests', TestRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const privateKey = fs.readFileSync('src/key.pem');
-const certificate = fs.readFileSync('src/cert.pem');
+// const privateKey = fs.readFileSync('src/key.pem');
+// const certificate = fs.readFileSync('src/cert.pem');
 
-const httpsServer = https.createServer({
-  key: privateKey,
-  cert: certificate
-}, app);
-httpsServer.listen(httpsport, () => {
-  console.log(`Example app listening at https://localhost:${httpsport}`);
+// const httpsServer = https.createServer({
+//   key: privateKey,
+//   cert: certificate
+// }, app);
+// httpsServer.listen(httpsport, () => {
+//   console.log(`Example app listening at https://localhost:${httpsport}`);
+//   console.log(env.NODE_ENV);
+// });
+
+// const httpServer = http.createServer((req, res) => {
+//   const redirectUrl = `https://localhost:${httpsport}${req.url}`;
+//   res.writeHead(301, { "Location": redirectUrl });
+//   res.end();
+// });
+// httpServer.listen(httpport, () => {
+//   console.log(`HTTP Server running on port ${httpport} and redirecting to HTTPS`);
+// });
+
+app.listen(httpport, () => {
+  console.log(`Example app listening on port ${httpport}`);
   console.log(env.NODE_ENV);
-});
-
-const httpServer = http.createServer((req, res) => {
-  const redirectUrl = `https://localhost:${httpsport}${req.url}`;
-  res.writeHead(301, { "Location": redirectUrl });
-  res.end();
-});
-httpServer.listen(httpport, () => {
-  console.log(`HTTP Server running on port ${httpport} and redirecting to HTTPS`);
 });
