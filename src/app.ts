984aa893a73c5a556f4c4f2cf3d6e4ec395dec47
final898y <final898y@gmail.com>
2024-12-28 00:52:58 +0800

    feat. 增加cors、axios、增加express.static

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import fs from 'fs';
 import https from 'https';
 import http from 'http';
+import cors from 'cors';
+import path from 'path';
 
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
@@ -18,6 +20,17 @@ const httpsport: number = env.HTTPSPORT;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+const corsOptions = {
+  origin: [
+    'http://localhost:3000',
+    'https://localhost:443',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
@@ -25,6 +38,10 @@ app.use('/', IndexRouter);
 app.use('/users', UserRouter);
 app.use('/tests', TestRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/axios', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'axiosTest.html'));
+});
 
 const privateKey = fs.readFileSync('src/key.pem');
 const certificate = fs.readFileSync('src/cert.crt');
